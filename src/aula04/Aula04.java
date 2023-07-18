cc7e9225570fc8d8dafc8a5a9e631be1a4565667
Gabriel.M <gamotasantos2000@gmail.com>
2023-07-18 09:33:46 -0400

    metodos getter, setter e constructor

---
+++ src/aula04/Aula04.java
@@ -0,0 +1,10 @@
+package aula04;
+
+public class Aula04 {
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta("FINEPEN", "Preta", 0.4f);
+        c1.status();
+        Caneta c2 = new Caneta("INJEXPEN", "Azul", 1.0f);
+        c2.status();
+    }
+}
